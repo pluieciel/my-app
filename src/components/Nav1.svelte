f3d7da6500b25abbf3eacb5b7d0468f99c7a5358
yue <yorke5@qq.com>
2020-05-29 12:06:24 +0200

    nav

--- src/components/Nav1.svelte
+++ src/components/Nav1.svelte
@@ -1,10 +1,12 @@
 <script>
     export let y;
-    
+    let x = false;
+    const handleMouseenter = () => x = true;
+	const handleMouseleave = () => x = false;
 </script>
 
 
-<nav class="flex w-full z-40 fixed items-center justify-between pr-4 bg-gray-800 {y < 10 ? '' : 'opacity-75'}">
+<nav class="flex w-full z-30 fixed items-center justify-between pr-4 bg-gray-800 {y < 10 ? '' : 'opacity-75'}">
     <div></div>
 
     <div class="flex items-center p-2">
@@ -15,7 +17,9 @@
             </p>
         </a>
 
-        <a href="/louvre" class="flex hover:opacity-25 ml-6">
+        <a href="/" class="flex hover:opacity-25 ml-6"
+        on:mouseenter={handleMouseenter}
+	    on:mouseleave={handleMouseleave}>
             <img class="w-4 " src="http://www.entypo.com/images/address.svg" alt="">
             <p class="font-medium text-gray-300 text-sm ml-1">
             Sites
@@ -37,3 +41,15 @@
 
     </div>
 </nav>
+
+<div class="flex w-full justify-end"
+on:mouseenter={handleMouseenter}
+on:mouseleave={handleMouseleave}>
+    <div class="flex flex-col fixed z-40 mr-56 {x ? '' : 'hidden'} mt-8">
+        <div class="h-2"></div>
+        <div class="flex flex-col bg-gray-800 bg-opacity-75 p-2">
+            <a href="/louvre" class="font-medium text-gray-300 text-sm m-2 hover:opacity-25">Louvre</a>
+            <a href="/" class="font-medium text-gray-300 text-sm m-2 hover:opacity-25">Eiffel</a>
+        </div>
+    </div>
+</div>
--- src/routes/louvre.svelte
+++ src/routes/louvre.svelte
@@ -2,6 +2,6 @@
 	<title>Louvre</title>
 </svelte:head>
 
-<h1>About this site</h1>
-
+<div class="h-10"></div>
+<img class="w-full" src="https://images.unsplash.com/photo-1560425946-7d5830202765?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" alt="">
 <p class="text-gray-100">This is the 'about' page. There's not much here.</p>
