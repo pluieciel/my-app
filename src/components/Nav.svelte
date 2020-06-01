8342a36f7d4f0085153ac181f80c94fe3ae8beb0
yue <yorke5@qq.com>
2020-06-01 09:42:42 +0200

    Revert "change 0601"
    This reverts commit b2e81ddbab264ab5bfee9755293de7baca1f6a9a.

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,5 +1,11 @@
 <script>
 	export let segment;
+
+	import CustomButton from '../components/CustomButton.svelte';
+
+	function handleClick() {
+		alert('clicked');
+	}
 </script>
 
 <style>
@@ -8,48 +14,56 @@
 		font-weight: 300;
 		padding: 0 1em;
 	}
+
 	ul {
 		margin: 0;
 		padding: 0;
 	}
+
 	/* clearfix */
 	ul::after {
 		content: '';
 		display: block;
 		clear: both;
 	}
+
 	li {
 		display: block;
 		float: left;
 	}
+
 	[aria-current] {
 		position: relative;
 		display: inline-block;
 	}
+
 	[aria-current]::after {
 		position: absolute;
 		content: '';
-		width: calc(100% - 1em);
+		width: calc(100% - 0.5em);
 		height: 2px;
-		background-color: rgb(255,62,0);
+		background-color: rgb(212, 41, 41);
 		display: block;
 		bottom: -1px;
 	}
+
 	a {
 		text-decoration: none;
-		padding: 1em 0.5em;
+		margin: 0.5em 0.25em;
+		padding: 0.5em 0.25em;
 		display: block;
 	}
 </style>
 
-<nav>
-	<ul>
-		<li><a aria-current='{segment === undefined ? "page" : undefined}' href='.'>home</a></li>
-		<li><a aria-current='{segment === "story" ? "page" : undefined}' href='story'>story</a></li>
-
+<nav class="bg-gray-100">
+	<ul class="flex flex-col">
+		<li><a class="hover:bg-red-200" aria-current='{segment === undefined ? "page" : undefined}' href='.'>home</a></li>
+		<li><a class="hover:bg-red-200" aria-current='{segment === "louvre" ? "page" : undefined}' href='louvre'>louvre</a></li>
+		<li><CustomButton on:click={handleClick} text={"Louvre"}/></li>
 		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch aria-current='{segment === "site" ? "page" : undefined}' href='site'>site</a></li>
-		<li><a aria-current='{segment === "garden" ? "page" : undefined}' href='garden'>garden</a></li>
+		<!--
+		<li><a rel=prefetch aria-current='{segment === "blog" ? "page" : undefined}' href='blog'>blog</a></li>
+	    -->
 	</ul>
 </nav>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,114 +1,20 @@
-<!DOCTYPE HTML>
-	<svelte:head>
-		<title>Travel to Paris</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<link rel="stylesheet" href="static/main.css" />
-	</svelte:head>
-	<body>
-		<div class="page-wrap">
+<script>
 
-			<!-- Main -->
-				<section id="main">
+</script>
 
-					<!-- Banner -->
-						<section id="banner">
-							<div class="inner">
-								<h1>Welcome to Paris</h1>
-								<p>A website give you the best navigation. </p>
-								<ul class="actions">
-									<li><a href="#sites" class="button alt scrolly big">Continue</a></li>
-								</ul>
-							</div>
-						</section>
+<style>
 
-					<!-- Site -->
-						<section id="sites">
+</style>
 
-							<!-- Photo Galleries -->
-								<div class="site">
-									<header class="special">
-										<h2>Top 8</h2>
-									</header>
-									<div class="content">
-										<div class="media">
-											<a href="Arc_de_Triomphe.jpg"><img src="Arc_de_Triomphe.jpg" alt="" title="This right here is a caption." /></a>
-										</div>
-										<div class="media">
-											<a href="effel_tower.jpg"><img src="effel_tower.jpg" alt="" title="This right here is a caption." /></a>
-										</div>
-										<div class="media">
-											<a href="Louvre.jpg"><img src="Louvre.jpg" alt="" title="This right here is a caption." /></a>
-										</div>
-										<div class="media">
-											<a href="notre_dame.jpg"><img src="notre_dame.jpg" alt="" title="This right here is a caption." /></a>
-										</div>
-										<div class="media">
-											<a href="opera.jpg"><img src="opera.jpg" alt="" title="This right here is a caption." /></a>
-										</div>
-										<div class="media">
-											<a href="sacre_coeur.jpg"><img src="sacre_coeur.jpg" alt="" title="This right here is a caption." /></a>
-										</div>
-										<div class="media">
-											<a href="jardin_louxembourg.jpg"><img src="jardin_louxembourg.jpg" alt="" title="This right here is a caption." /></a>
-										</div>
-										<div class="media">
-											<a href="versailles.jpg"><img src="versailles.jpg" alt="" title="This right here is a caption." /></a>
-										</div>
-									</div>
-									<footer>
-										<a href="louvre.svelte" class="button big">Learn More</a>
-									</footer>
-								</div>
-						</section>
-
-					<!-- Contact -->
-						<section id="contact">
-							<!-- Social -->
-								<div class="social column">
-									<h3>About Us</h3>
-									<p>We are 4 ESSEC students.</p>
-									<p>Actively promoting the beauty of Paris to the world</p>
-									<h3>Follow Me</h3>
-									<ul class="icons">
-										<li><a href="https://twitter.com/explore" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-										<li><a href="https://www.facebook.com/" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-										<li><a href="https://www.instagram.com/?hl=zh-tw" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-									</ul>
-								</div>
-
-							<!-- Form -->
-								<div class="column">
-									<h3>Get in Touch</h3>
-									<form action="#" method="post">
-										<div class="field half first">
-											<label for="name">Name</label>
-											<input name="name" id="name" type="text" placeholder="Name">
-										</div>
-										<div class="field half">
-											<label for="email">Email</label>
-											<input name="email" id="email" type="email" placeholder="Email">
-										</div>
-										<div class="field">
-											<label for="message">Message</label>
-											<textarea name="message" id="message" rows="6" placeholder="Message"></textarea>
-										</div>
-										<ul class="actions">
-											<li><input value="Send Message" class="button" type="submit"></li>
-										</ul>
-									</form>
-								</div>
-
-						</section>
-
-					<!-- Footer -->
-						<footer id="footer">
-							<div class="copyright">
-								&copy; Untitled Design: <a href="https://templated.co/">TEMPLATED</a>. Images: <a href="https://unsplash.com/">Unsplash</a>.
-							</div>
-						</footer>
-				</section>
-		</div>
+<svelte:head>
+	<title>Welcome to Paris</title>
+</svelte:head>
 
 
-	</body>
+
+
+<div class="flex flex-col">
+	<div class=""></div>
+	<img class="m-auto w-full" src="https://images.unsplash.com/photo-1431274172761-fca41d930114?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" alt="">
+	<p class="text-gray-100 m-auto h-screen">test</p>
+</div>
