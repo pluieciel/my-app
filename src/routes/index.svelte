a672e8d98cece0de5f2cacdaf8d4a0ca56566296
yue <yorke5@qq.com>
2020-06-01 15:10:16 +0200

    add css in index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -38,14 +38,14 @@
 					</section>
 
 				<!-- Site -->
-					<section id="sites">
+					<section id="sites" class="flex flex-col">
 
 						<!-- Photo Galleries -->
-							<div class="site">
-								<header class="special">
-									<h2>Top 8</h2>
+							<div class="flex flex-col items-center">
+								<header class="h-20 p-3">
+									<p class="text-white text-3xl">Top 8</p>
 								</header>
-								<div class="content">
+								<div class="grid grid-cols-4">
 									<div class="media">
 										<a href="Arc_de_Triomphe.jpg"><img src="Arc_de_Triomphe.jpg" alt="" title="This right here is a caption." /></a>
 									</div>
@@ -71,21 +71,21 @@
 										<a href="versailles.jpg"><img src="versailles.jpg" alt="" title="This right here is a caption." /></a>
 									</div>
 								</div>
-								<footer>
-									<a href="louvre.svelte" class="button big">Learn More</a>
-								</footer>
+								<a href="/" class="mt-10">
+									<button class="rounded bg-blue-600 hover:bg-blue-800 text-white p-2 pl-5 pr-5">Learn More</button>
+								</a>
 							</div>
 					</section>
 
 				<!-- Contact -->
-					<section id="contact">
+					<section id="contact" class="mt-5 grid grid-cols-2 h-64">
 						<!-- Social -->
-							<div class="social column">
-								<h3>About Us</h3>
+							<div class="flex flex-col bg-gray-300 p-10 text-gray-900">
+								<p class="font-bold text-xl mb-3">About Us</p>
 								<p>We are 4 ESSEC students.</p>
 								<p>Actively promoting the beauty of Paris to the world</p>
-								<h3>Follow Me</h3>
-								<ul class="icons">
+								<p class="font-bold text-xl mt-5">Follow Me</p>
+								<ul class="mt-1">
 									<li><a href="https://twitter.com/explore" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
 									<li><a href="https://www.facebook.com/" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
 									<li><a href="https://www.instagram.com/?hl=zh-tw" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
@@ -93,23 +93,28 @@
 							</div>
 
 						<!-- Form -->
-							<div class="column">
-								<h3>Get in Touch</h3>
-								<form action="#" method="post">
-									<div class="field half first">
-										<label for="name">Name</label>
-										<input name="name" id="name" type="text" placeholder="Name">
+							<div class="bg-gray-400 p-10 text-gray-900">
+								<p class="font-bold text-xl mb-3">Get in Touch</p>
+								<form action="#" method="post" class="grid grid-cols-2 gap-4">
+									<div class="flex flex-col">
+										<label for="name" class="font-bold mb-3">Name</label>
+										<input name="name" id="name" type="text" placeholder="  Name"
+											class="placeholder-blue-400 border rounded">
 									</div>
-									<div class="field half">
-										<label for="email">Email</label>
-										<input name="email" id="email" type="email" placeholder="Email">
+									<div class="flex flex-col">
+										<label for="email" class="font-bold mb-3">Email</label>
+										<input name="email" id="email" type="email" placeholder="  Example@email.com"
+										    class="placeholder-blue-400 border rounded">
 									</div>
-									<div class="field">
-										<label for="message">Message</label>
-										<textarea name="message" id="message" rows="6" placeholder="Message"></textarea>
+									<div class="flex flex-col col-span-2">
+										<label for="message"  class="font-bold mb-3">Message</label>
+										<textarea name="message" id="message" rows="6" placeholder="  Message"
+										    class="placeholder-blue-400 border rounded"></textarea>
 									</div>
 									<ul class="actions">
-										<li><input value="Send Message" class="button" type="submit"></li>
+										<li><input value="Send Message" 
+											class="rounded bg-blue-600 hover:bg-blue-800 text-white p-2 pl-5 pr-5 mt-4" 
+											type="submit"></li>
 									</ul>
 								</form>
 							</div>
@@ -118,9 +123,7 @@
 
 				<!-- Footer -->
 					<footer id="footer">
-						<div class="copyright">
-							&copy; Untitled Design: <a href="https://templated.co/">TEMPLATED</a>. Images: <a href="https://unsplash.com/">Unsplash</a>.
-						</div>
+						
 					</footer>
 			</section>
 	</div>
